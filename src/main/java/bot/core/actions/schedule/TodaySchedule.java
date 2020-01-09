15d87a97f32bc1744cea55c65fdc0fb168b0a227
NikitaTr99 <NikitaTr99@yandex.ru>
2020-01-09 15:31:31 +0400

    update action for new subject utils

--- src/main/java/bot/core/actions/schedule/TodaySchedule.java
+++ src/main/java/bot/core/actions/schedule/TodaySchedule.java
@@ -2,6 +2,7 @@ package bot.core.actions.schedule;
 
 import bot.Bootstrapper;
 import bot.core.actions.Action;
+import bot.core.actions.daily.utils.SubjectManager;
 import bot.vkcore.VKManager;
 import com.vk.api.sdk.objects.messages.Message;
 
@@ -28,58 +29,10 @@ public class TodaySchedule extends Action {
 
     @Override
     public void execute(Message message) {
-        new VKManager().sendMessage(buildMessage(GetTodaySchedule(),message.getPeerId()));
+        new VKManager().sendMessage(buildMessage("Сегодня "
+                + LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, localeRu) + ": \n" +
+                        new SubjectManager().TodaySchedule()
+                ,message.getPeerId()));
         LogToConsole(log(message));
     }
-
-
-    private String GetTodaySchedule(){
-        return Bootstrapper.BotSettings.bot_properties
-                .getProperty("path_to_days").equals("null")? TodayScheduleSource() : TodaySchedulePath();
-    }
-
-    private String TodayScheduleSource(){
-        return "Сегодня " +
-                LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, localeRu) + ": \n" +
-                GetBuildedSubjects(Objects.requireNonNull(
-                        ClassLoader.getSystemResourceAsStream(
-                                path_to_days + LocalDate.now().getDayOfWeek().toString().toLowerCase() + ".txt"
-                        )));
-    }
-
-    private String TodaySchedulePath() {
-        try {
-            return "Сегодня " +
-                    LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, localeRu) + ": \n" +
-                    GetBuildedSubjects(new FileInputStream(new File(path_to_days
-                            + "/"
-                            + LocalDate.now().getDayOfWeek().toString().toLowerCase()
-                            + ".txt"
-                    )));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-
-    private String GetBuildedSubjects(InputStream inputStream){
-
-        String newLine = System.getProperty("line.separator");
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-            StringBuilder result = new StringBuilder();
-            String line; int counter = 1;
-            while ((line = reader.readLine()) != null) {
-                result.append(counter)
-                        .append(". ").append(line)
-                        .append(newLine);
-                counter++;
-            }
-            return result.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 }
