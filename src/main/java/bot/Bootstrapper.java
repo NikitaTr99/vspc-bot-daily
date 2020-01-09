1678b19291506007b28d4a01e97d072e8cddabb6
NikitaTr99 <NikitaTr99@yandex.ru>
2020-01-09 14:18:31 +0400

    new newsletter subscription feature

--- src/main/java/bot/Bootstrapper.java
+++ src/main/java/bot/Bootstrapper.java
@@ -1,12 +1,16 @@
 package bot;
 
+import bot.core.interfaces.Loggable;
 import bot.vkcore.VKCore;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.Executors;
@@ -36,6 +40,7 @@ public class Bootstrapper {
     public static class BotSettings {
         public static Properties bot_properties;
         public static Properties vk_properties;
+        public static ArrayList<Integer> subscribers;
         static {
             System.out.println("Load configuration...");
             bot_properties = new Properties();
@@ -62,6 +67,7 @@ public class Bootstrapper {
                     System.out.println("Error loading VK configuration.");
                 }
             }
+            subscribers = LoadSubscribers();
         }
         private static Properties loadFromFile(String name) throws IOException {
             Properties p = new Properties();
@@ -74,6 +80,28 @@ public class Bootstrapper {
             p.load(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(name)));
             System.out.println(name + " load from source.");
             return p;
+        }
+        private static ArrayList<Integer> LoadSubscribers(){
+            String property = BotSettings.bot_properties.getProperty("subscribers");
+            ArrayList<Integer> subscribers = new ArrayList<>();
+            if(property.isEmpty()){
+                return null;
+            }
+            for (String s : property.split(";")){
+                subscribers.add(Integer.parseInt(s));
+            }
+            return subscribers;
+        }
+        public static void AddSubscriber(Integer id){
+            String property = BotSettings.bot_properties.getProperty("subscribers");
+            property += id + ";";
+            bot_properties.setProperty("subscribers", property);
+            try {
+                bot_properties.store(new FileOutputStream("bot-config.properties"),"new subscriber");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error updating configuration.");
+            }
         }
     }
 }
